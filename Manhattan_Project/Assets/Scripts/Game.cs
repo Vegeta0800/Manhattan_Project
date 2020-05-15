1be65f5b3e7d8f2a7e55ed9a9d652ea984bc8328
Raffi <raffikaul@outlook.com>
2020-05-15 23:03:00 +0200

    Added Materials, Changed Scene and Added Scripts
    [+] multiple prototype materials for fields and players
    [+] added prototype soldier prefabs
    [+] added Game and Player scripts
    [*] added playing fields and factories into scene

---
+++ Manhattan_Project/Assets/Scripts/Game.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Player[] players;
+    [SerializeField] private Transform gameField;
+
+    public static Material selectedMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players[0] = new Player();
+        players[1] = new Player();
+
+        players[0].Init(10, 5, null, GetAllChildren(GetChildByTag(gameField, "P1_Fields")), new List<string>() { "P2_Fields" });
+        players[1].Init(10, 5, null, GetAllChildren(GetChildByTag(gameField, "P2_Fields")), new List<string>() { "P1_Fields" });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Player player in players)
+        {
+            //BUYPHASE
+            while(player.GetPhase() == Player.PlayerPhase.BUY)
+            {
+                player.BuyPhase();
+            }
+            //COMBAT MOVEMENT
+            while (player.GetPhase() == Player.PlayerPhase.COMBATMOV)
+            {
+                player.CombatMovPhase();
+            }
+            //COMBAT
+            //NON COMBAT MOVEMENT
+            //SET TROOPS
+        }
+    }
+    
+    Transform GetChildByTag(Transform t, string tag)
+    {
+        if (t.childCount == 0) return null;
+
+        for (int i = 0; i < t.childCount; i++)
+        {
+            if (t.GetChild(i).tag == tag)
+                return t.GetChild(i);
+        }
+
+        return null;
+    }
+    List<Transform> GetAllChildren(Transform t)
+    {
+        if (t.childCount == 0) return null;
+
+        List<Transform> children = new List<Transform>();
+
+        for(int i = 0; i < t.childCount; i++)
+        {
+            children.Add(t.GetChild(i));
+        }
+
+        return children;
+    }
+}
---
+++ Manhattan_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    public enum PlayerPhase
+    {
+        BUY = 0,
+        COMBATMOV = 1,
+        FIGHT = 2,
+        NONCOMBATMOV = 3,
+        SET = 4,
+        ENDMOVE = 5
+    }
+
+    private int money;
+    private int income;
+
+    private List<string> enemyfieldTags;
+
+    private List<Player> allies;
+    private List<Transform> fields;
+
+    private List<Transform> units;
+    private List<int> boughtUnits;
+    private PlayerPhase currentPhase;
+
+    public void Init(int startMoney, int incomeIn, List<Player> alliesIn, List<Transform> fieldsIn, List<string> enemyfieldTagsIn)
+    {
+        money = startMoney;
+        income = incomeIn;
+        enemyfieldTags = enemyfieldTagsIn;
+
+        if(alliesIn != null)
+            allies = alliesIn;
+        if (fieldsIn != null)
+            fields = fieldsIn;
+    }
+
+    public void BuyPhase()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Bought Soldier for 1");
+            boughtUnits.Add(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Bought Tank for 2");
+            boughtUnits.Add(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("Bought Plane for 3");
+            boughtUnits.Add(2);
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            currentPhase = PlayerPhase.COMBATMOV;
+        }
+    }
+
+    public void CombatMovPhase()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (enemyfieldTags.Contains(hit.transform.tag))
+                {
+                    hit.transform.GetComponent<MeshRenderer>().material = Game.selectedMat;
+                }
+                else
+                {
+                }
+            }
+        }
+    }
+
+    public PlayerPhase GetPhase()
+    {
+        return currentPhase;
+    }
+}
